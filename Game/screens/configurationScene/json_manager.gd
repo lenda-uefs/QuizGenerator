7c3c4744703e7398e1d07838dfa0fd73222a11f4
Victória Gomes <38071157+victoriaogomes@users.noreply.github.com>
2019-06-18 15:22:26 -0300

    abc

--- Game/screens/configurationScene/json_manager.gd
+++ Game/screens/configurationScene/json_manager.gd
@@ -4,6 +4,8 @@ extends Node
 var tex
 #warning-ignore:unassigned_variable
 var img
+var dir
+var nfiles
 
 func _ready():
 	$FileDialog.set_filters(PoolStringArray(["*.json"]))
@@ -14,17 +16,61 @@ func _ready():
 	yield($AnimationPlayer, "animation_finished")
 	$AnimationPlayer.play("logo_leaving", -1, 1.0, false)
 	yield($AnimationPlayer, "animation_finished")
-	$AnimationPlayer.play("opening", -1, 1.0, false)
-	yield($AnimationPlayer, "animation_finished")
+	nfiles = _count_old_configFiles()
+	if(nfiles == 0):
+		$AnimationPlayer.play("opening", -1, 1.0, false)
+		yield($AnimationPlayer, "animation_finished")
+	elif(nfiles == 1):
+		_on_FileDialog_file_selected("user://config_files/file.json")
 
 
 func load_dialogue(file_path) -> Dictionary:
 	var file = File.new()
 	assert file.file_exists(file_path)
+	if(nfiles==0):
+		dir = Directory.new()
+		dir.copy(file_path, "user://config_files/file.json")
 	file.open(file_path, file.READ)
 	var dialogue = parse_json(file.get_as_text())
 	assert dialogue.size()>0
 	return dialogue
+
+
+func delete_old_Files():
+	dir = Directory.new()
+	if dir.open("user://images") == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while (file_name != ""):
+			if(!dir.current_is_dir()):
+				print("Found file: " + file_name)
+				dir.remove(file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path pra apagar.")
+
+
+func _count_old_configFiles() -> int:
+	dir = Directory.new()
+	var nFiles = 0
+	if(!dir.dir_exists("user://config_files")):
+		dir.open("user://")
+		dir.make_dir("config_files")
+	if(!dir.dir_exists("user://images")):
+		dir.open("user://")
+		dir.make_dir("images")
+	else:
+		if dir.open("user://config_files") == OK:
+			dir.list_dir_begin()
+			var file_name = dir.get_next()
+			while (file_name != ""):
+				if(!dir.current_is_dir()):
+					nFiles = nFiles+1
+					print("Found file: " + file_name)
+				file_name = dir.get_next()
+		else:
+			print("An error occurred when trying to access the path pra contar.")
+	return nFiles
 
 
 func _on_carregarArquivo_pressed():
@@ -33,10 +79,14 @@ func _on_carregarArquivo_pressed():
 
 
 func _on_FileDialog_file_selected(path):
-	$AnimationPlayer.play_backwards("opening", -1)
-	yield($AnimationPlayer, "animation_finished")
-	$Popup.popup_centered()
-	$AnimationPlayer.play("configuration", -1, 1.0, false)
+	if(nfiles == 0):
+		$AnimationPlayer.play_backwards("opening", -1)
+		yield($AnimationPlayer, "animation_finished")
+		$Popup.popup_centered()
+		$AnimationPlayer.play("configuration", -1, 1.0, false)
+	else:
+		$Popup2.popup_centered()
+		$AnimationPlayer.play("configuration2", -1, 1.0, false)
 	var dic = load_dialogue(path)
 	_set_game_configuration(dic)
 
@@ -57,10 +107,46 @@ func _set_game_configuration(dic):
 		global_config.game_color["b"] = 0.96
 		global_config.game_color["a"] = 1
 	if(!dic["Title_screen_background"].empty()):
-		global_config.title_screen_background = _load_process(tex, img, dic["Title_screen_background"])
+		if(nfiles>0):
+			var path = dic["Title_screen_background"]
+			path.erase(0,path.find_last("/")+1)
+			path = "user://images/" + path
+			global_config.title_screen_background = _load_process(tex, img, path)
+		else:
+			global_config.title_screen_background = _load_process(tex, img, dic["Title_screen_background"])
 	else:
 		global_config.title_screen_background = null
-	_importImages()
+	if(nfiles>0):
+		_importInsideImages()
+	else:
+		_importImages()
+
+
+func _importInsideImages():
+	var path
+	for h in range(1, global_config.stories.size()+1):
+		for k in range (1, global_config.stories["Story_" + str(h)]["Questions"].size()+1):
+			for j in range (1, 3):
+				path = global_config.stories["Story_" + str(h)]["Questions"]["Question_" + str(k)]["option_" + str(j)]
+				path.erase(0,path.find_last("/")+1)
+				path = "user://images/" + path
+				print(path)
+				global_config.stories["Story_" + str(h)]["Questions"]["Question_" + str(k)]["option_" + str(j)] = _load_process(tex, img, path) 
+		path = global_config.stories["Story_" + str(h)]["Story_cover"]
+		path.erase(0,path.find_last("/")+1)
+		path = "user://images/" + path
+		global_config.stories["Story_" + str(h)]["Story_cover"] = _load_process(tex, img, path)
+	var next_screen = load("res://screens/titleScreen/titleScreen.tscn")
+	yield(get_tree().create_timer(2), "timeout")
+	if(nfiles==0):
+		$AnimationPlayer.play("config_done", -1, 1.0, false)
+	else:
+		$AnimationPlayer.play("config_done2", -1, 1.0, false)
+	yield($AnimationPlayer, "animation_finished")
+	yield(get_tree().create_timer(0.6), "timeout")
+	get_tree().call_group("managers", "loading_process_ended")
+	#warning-ignore:return_value_discarded
+	get_tree().change_scene_to(next_screen)
 
 
 func _importImages():
@@ -80,6 +166,12 @@ func _importImages():
 
 
 func _load_process(tex, img, loadPath) -> ImageTexture:
+	var extension = loadPath
+	extension.erase(0,extension.find_last("/")+1)
+	print("Extensão:" + extension)
+	if(nfiles == 0):
+		dir = Directory.new()
+		dir.copy(loadPath, "user://images/" + extension)
 	tex = ImageTexture.new()
 	img = Image.new()
 	img.load(loadPath)
@@ -88,7 +180,7 @@ func _load_process(tex, img, loadPath) -> ImageTexture:
 
 
 func _on_baixarModelo_pressed():
-	var dir = Directory.new()
+	dir = Directory.new()
 	dir.copy("res://model.json", "C://Users/Public/Downloads/model.json")
 	#warning-ignore:return_value_discarded
 	OS.shell_open("C://Users/Public/Downloads")
